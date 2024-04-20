72319236f74b90cabcf8a051e0992913a8c74e71
saravr <saravr@yahoo.com>
2024-04-20 10:24:49 -0700

    Make vm use use case

--- app/src/main/java/com/example/helloroom/viewmodel/DocumentViewModel.kt
+++ app/src/main/java/com/example/helloroom/viewmodel/DocumentViewModel.kt
@@ -1,50 +1,19 @@
 package com.example.helloroom.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.helloroom.model.Document
-import com.example.helloroom.repository.DocumentRepository
+import com.example.helloroom.domain.GetDocumentsUseCase
+//import com.example.helloroom.domain.UpdateSavedUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class DocumentViewModel @Inject constructor(
-    private val documentRepository: DocumentRepository,
+    private val getDocumentsUseCase: GetDocumentsUseCase,
+    //private val updateSavedUseCase: UpdateSavedUseCase,
 ): ViewModel() {
-    private val _documents = MutableStateFlow<List<Document>>(emptyList())
-    val documents = _documents.asStateFlow()
+    val documents = getDocumentsUseCase.documents
 
-    init {
-        viewModelScope.launch {
-            val combinedFlow = combine(
-                documentRepository.documents,
-                documentRepository.saved,
-            ) { d, s ->
-                Timber.e("++++ VM: TRANSFORM ${d.size} / ${s.size}")
-                Pair(d, s)
-            }
+    suspend fun refresh() = {} //documentRepository.refresh()
 
-            combinedFlow.collectLatest { (documentsList, savedList) ->
-                Timber.e("++++ VM: COLLECT: DOCS ${documentsList.size}, SAVED ${savedList.size}")
-                val newList = documentsList.map {
-                    savedList.firstOrNull { saved -> saved.documentId == it.id }?.let { saved ->
-                        it.copy(saved = saved.saved)
-                    } ?: run {
-                        it
-                    }
-                }
-                _documents.emit(newList)
-            }
-        }
-    }
-
-    suspend fun refresh() = documentRepository.refresh()
-
-    suspend fun saveDocument(documentId: Int, state: Boolean): Int = documentRepository.saveDocument(documentId, state)
+    suspend fun saveDocument(documentId: Int, state: Boolean): Int = getDocumentsUseCase.saveDocument(documentId, state)
 }
