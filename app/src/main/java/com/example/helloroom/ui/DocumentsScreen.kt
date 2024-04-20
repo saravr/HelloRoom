b4444bedd3ba288adcf2d469bea4d3abc39880ab
saravr <saravr@yahoo.com>
2024-04-20 00:53:02 -0700

    Move packages

--- app/src/main/java/com/example/helloroom/ui/DocumentsScreen.kt
+++ app/src/main/java/com/example/helloroom/ui/DocumentsScreen.kt
@@ -1,4 +1,4 @@
-package com.example.helloroom
+package com.example.helloroom.ui
 
 import android.widget.Toast
 import androidx.compose.foundation.clickable
--- app/src/main/java/com/example/helloroom/ui/MainActivity.kt
+++ app/src/main/java/com/example/helloroom/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.helloroom
+package com.example.helloroom.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
