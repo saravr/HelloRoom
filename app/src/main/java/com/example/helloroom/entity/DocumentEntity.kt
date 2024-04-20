aed9b33881494c709bc0c0af118ad1c541e99df3
saravr <saravr@yahoo.com>
2024-04-19 19:15:23 -0700

    Use Ignore field in doc entity

--- app/src/main/java/com/example/helloroom/entity/DocumentEntity.kt
+++ app/src/main/java/com/example/helloroom/entity/DocumentEntity.kt
@@ -2,6 +2,7 @@ package com.example.helloroom.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import com.example.helloroom.model.Document
 
@@ -15,18 +16,21 @@ data class DocumentEntity(
     val author: String,
     @ColumnInfo("page_count")
     val pageCount: Int,
-    @ColumnInfo("saved")
-    val saved: Boolean,
 ) {
+    @Ignore
+    private var saved: Boolean = false
+
     fun fromEntity(): Document {
         return Document(id, title, author, pageCount, saved)
     }
 
     companion object {
         fun toEntity(document: Document): DocumentEntity {
-            return DocumentEntity(
-                document.id, document.title, document.author, document.pageCount, document.saved
+            val entity = DocumentEntity(
+                document.id, document.title, document.author, document.pageCount
             )
+            entity.saved = document.saved
+            return entity
         }
     }
 }
